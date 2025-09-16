4ed06917dfcded70e4a8272218f9ff538d3952b8
Kris Ilowiecki <krymzon@gmail.com>
2025-09-16 20:44:31 +0200

    try non-themery structure

--- lua/kimiko/hi.lua
+++ lua/kimiko/hi.lua
@@ -8,7 +8,7 @@ local groups = {
     LineNr = { fg = p.line },
     CursorLine = { bg = p.bg1 },
     ColorColumn = { bg = p.bg0 },
-    -- Add more
+    -- More: CursorColumn = { bg = p.bg2 },
   },
   syntax = {
     Comment = { fg = p.fgc },
@@ -35,32 +35,35 @@ local groups = {
     ["@number"] = { link = "Number" },
     ["@operator"] = { link = "Operator" },
     ["@comment"] = { link = "Comment" },
-    -- More @groups
+    -- More: ["@punctuation"] = { fg = p.op },
   },
   ui = {
     Pmenu = { fg = p.ui_menu, bg = p.bg3 },
     PmenuSel = { fg = p.fg2, bg = p.bg_sel },
     StatusLine = { fg = p.ui_status, bg = p.bg_ui },
-    StatusLineNC = { fg = p.fedece, bg = p.bg_ui },  -- Add #fedece if missing in palette
+    StatusLineNC = { fg = "#fedece", bg = p.bg_ui },  -- Add hex if needed
     TabLine = { fg = p.fg_dim, bg = p.bg_ui, underline = true },
-    TabLineFill = { fg = p.fg_dim, bg = p.bg_ui, underline = true },
-    TabLineSel = { fg = p.fg_dim, bg = p.bg_ui, underline = true },
+    TabLineFill = { fg = p.fg_dim, bg = "#4e4e52", underline = true },
+    TabLineSel = { fg = p.fg_dim, bg = "#00875f", underline = true },
     VertSplit = { fg = p.ui_border, bg = p.bg_ui },
     WinSeparator = { link = "VertSplit" },
     Search = { fg = p.fg0, bg = p.bg_search },
-    IncSearch = { fg = p.err, bg = p.bg_search },
+    IncSearch = { fg = "#670044", bg = "#8aff04" },
     Visual = { bg = p.bg_sel },
-    MatchParen = { bg = p.bg_sel },
+    MatchParen = { bg = "#005f00" },
     htmlH1 = { fg = p.title },
-    htmlH2 = { fg = p.title },
+    htmlH2 = { fg = "#ffdfdf" },
     htmlH3 = { fg = p.fg2, underline = true },
-    -- More
+    htmlH4 = { fg = p.fg3, underline = true },
+    htmlH5 = { fg = p.fg3 },
+    htmlH6 = { fg = p.fg4 },
+    -- More headings/UI
   },
   diff = {
     DiffAdd = { bg = p.bg_add },
     DiffChange = { bg = p.bg_chg },
     DiffDelete = { bg = p.bg_del },
-    DiffText = { bg = p.bg_chg },
+    DiffText = { bg = "#343466" },
   },
   plugins = {
     DiagnosticError = { fg = p.err },
@@ -69,25 +72,38 @@ local groups = {
     TelescopeSelection = { bg = p.bg_sel, fg = p.fg_sel },
     WhichKey = { fg = p.fg1 },
     NeoTreeNormal = { fg = p.fg1, bg = p.bg1 },
-    -- Extend
+    -- Extend: NeoTreeDirectoryIcon = { fg = p.succ },
   },
-  links = {  -- Pure links separate
+  links = {
     htmlTag = { link = "htmlTagName" },
     htmlEndTag = { link = "htmlSpecialTagName" },
     markdownCode = { link = "Number" },
   },
 }
 
--- Apply bold globally where needed
-local bold_groups = { "Conditional", "Statement", "Identifier", "Operator", "PreProc", "htmlH1", "htmlH2", "htmlH5", "pythonBuiltin", "pythonStatement", "VertSplit", "WinSeparator", "StatusLineNC" }
-local bold_underline_groups = { "Function", "TabLine", "TabLineFill", "TabLineSel", "Title", "Todo", "htmlH3", "htmlH4" }
+-- Bold groups (from gui)
+local bold_groups = {
+  "Conditional", "Define", "Exception", "Identifier", "Include",
+  "Operator", "PreProc", "Repeat", "Statement", "StatusLineNC",
+  "VertSplit", "WinSeparator", "htmlH1", "htmlH2", "htmlH5",
+  "pythonBuiltin", "pythonStatement",
+}
+local bold_underline_groups = {
+  "Function", "TabLine", "TabLineFill", "TabLineSel", "Title",
+  "Todo", "htmlH3", "htmlH4",
+}
 
 return {
   setup = function()
-    for cat, tbl in pairs(groups) do
+    for _, tbl in pairs(groups) do
       for name, opts in pairs(tbl) do
-        if vim.tbl_contains(bold_groups, name) then opts.bold = true end
-        if vim.tbl_contains(bold_underline_groups, name) then opts.bold = true; opts.underline = true end
+        if vim.tbl_contains(bold_groups, name) then
+          opts.bold = true
+        end
+        if vim.tbl_contains(bold_underline_groups, name) then
+          opts.bold = true
+          opts.underline = true
+        end
         hl(0, name, opts)
       end
     end
